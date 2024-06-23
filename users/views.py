41f1a449e6c30dc0cf97b7170d84249465fc10aa
RockRar484 <czileen484patel@gmail.com>
2024-06-24 00:23:03 +0530

    getting user from token done

--- users/views.py
+++ users/views.py
@@ -2,7 +2,32 @@ from django.shortcuts import render
 import datetime 
 from django.conf import settings
 import jwt
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 # Create your views here.
+
+def get_user_from_token(request):
+    authorization_header = request.META.get('HTTP_AUTHORIZATION')
+    if not authorization_header:
+        return Response({"error": "Authorization header is missing"}, status=status.HTTP_401_UNAUTHORIZED)
+    try:
+        token = authorization_header.split(' ')[1]
+        payload = jwt.decode(token, settings.SECRET_KEY, algorithms='HS256')
+        # Extract the id from the payload
+        user_id = payload.get('user_id')
+        # Return the id
+        return user_id
+    except jwt.ExpiredSignatureError:
+        return Response({"error": "Token has expired"}, status=status.HTTP_401_UNAUTHORIZED)
+    except jwt.InvalidTokenError:
+        return Response({"error": "Invalid token"}, status=status.HTTP_401_UNAUTHORIZED)
+    except IndexError:
+        return Response({"error": "Invalid token format"}, status=status.HTTP_401_UNAUTHORIZED)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+    
+
 def generate_jwt_token(user):
     payload = {
         'user_id': user.id,
