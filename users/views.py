4d39a1d9391c6f5620ff2fcd0f3521ea9567dbbf
RockRar484 <czileen484patel@gmail.com>
2024-06-24 00:21:26 +0530

    jwt token generation done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+import datetime 
+from django.conf import settings
+import jwt
 # Create your views here.
+def generate_jwt_token(user):
+    payload = {
+        'user_id': user.id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=24),
+        'iat': datetime.datetime.utcnow()
+    }
+    token = jwt.encode(payload, settings.SECRET_KEY, algorithm='HS256')
+    return token
